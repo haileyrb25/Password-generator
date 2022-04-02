d208a4e02b628c3ae6f5c4c330914bdd21301781
Hailey Bates <hailey.bates001@gmail.com>
2022-04-02 15:56:33 -0400

    Made slight changes to alert window

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
 const numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
+//window for first prompt to ask for length of characters
 var lengthPW = window.prompt(
   "Please enter the length of your password (between 8-128 characters)"
 );
 console.log(length);
-
+//this will determine if the person puts appropriate amount
 if (lengthPW > 7 && lengthPW < 129) {
   window.alert(
     "Your password length is " + lengthPW + ". Press OK to continue"
@@ -31,7 +31,6 @@ function writePassword() {
 }
 
 //ask if they want upper caser letters
-
 var passUp = confirm("Do you want to include upper case letters?")
 //ask if they want lower caser letters
 var passLow = confirm("Do you want to include lower case letters?")
