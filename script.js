2292071f87814cea7b083b8586b254078e16d871
Hailey Bates <hailey.bates001@gmail.com>
2022-04-07 23:50:46 -0400

    Added query selector for password to work

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
 const numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-//window for first prompt to ask for length of characters
+
+
+function generatePassword() {
+
 var lengthPW = window.prompt(
   "Please enter the length of your password (between 8-128 characters)"
 );
 console.log(length);
 //this will determine if the person puts appropriate amount
-if (lengthPW > 7 && lengthPW < 129) {
+if (lengthPW > 7 || lengthPW < 129) {
   window.alert(
     "Your password length is " + lengthPW + ". Press OK to continue"
   );
@@ -20,16 +23,9 @@ else {
   window.alert(
     "Password must be between 8-128 characters. Click OK to try again"
   );
-  generatePassword();
+  
 }
 // Write password to the #password input 
-function writePassword() {
-  passwordText.value = "";
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
 
 //ask if they want upper caser letters
 var passUp = confirm("Do you want to include upper case letters?")
@@ -55,10 +51,28 @@ if (passSpec) {
 if (passNumb) {
   finalComboArray = finalComboArray.concat(numberChar)
 }
+if (!passUp && !passLow && !passSpec && !passNumb){
+  alert("Your password does not contain any characters. Please choose characters for your password!")
+return null;
+} 
+
 
 
 console.log (finalComboArray)
+
+
+
 var word = "";
+for (let i = 0; i < lengthPW; i ++) {
 word += finalComboArray[Math.floor(Math.random() * finalComboArray.length)];
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
+return word
+}
+
+generateBtn.addEventListener("click", writepassword);
+function writepassword(){
+  var password = generatePassword();
+  var pwtext = document.querySelector("#password");
+  pwtext.value = password 
+}
